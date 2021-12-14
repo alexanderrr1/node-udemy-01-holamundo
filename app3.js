f6865bb5952636839a7dd8032ca3e260e76b80ad
Alexander Reineck <alexreineck@gmail.com>
2021-12-14 10:03:56 -0300

    Hola Mundo

---
+++ app3.js
@@ -0,0 +1,15 @@
+console.log("Inicio de programa"); // 1
+
+setTimeout(() => {
+    console.log("Primer Timeout"); // 5
+}, 3000);
+
+setTimeout(() => {
+    console.log("Segundo Timeout"); // 2
+}, 0);
+
+setTimeout(() => {
+    console.log("Tercer Timeout"); // 3
+}, 0);
+
+console.log("Fin de Programa"); // 4 :: 2
